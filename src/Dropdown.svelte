78ef16f12884a11427bb0c3074e13aafb877f9f9
Amjad-722 <aliamjad722ee@gmail.com>
2024-10-17 15:06:11 +0500

    updated

---
+++ src/Dropdown.svelte
@@ -0,0 +1,130 @@
+<script>
+  let searchQuery = "";
+  let isDropdownVisible = false;
+
+  // Function to toggle the dropdown visibility
+  function toggleDropdown() {
+    isDropdownVisible = !isDropdownVisible;
+  }
+
+  // Function to hide the dropdown when an option is clicked
+  function hideDropdown() {
+    isDropdownVisible = false;
+  }
+</script>
+
+<div>
+  <div class="flex flex-row justify-around">
+    <!-- Search Input -->
+    <div>
+      <input
+        type="search"
+        placeholder="Search"
+        bind:value={searchQuery}
+        class="w-full pl-10 pr-4 py-2 rounded-full border focus:outline-none focus:border-purple-500"
+      />
+    </div>
+
+    <!-- Dropdown Menu -->
+    <div class="">
+      <span
+        class="text-[20px] font-medium cursor-pointer"
+        
+      >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" on:click={toggleDropdown}>
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+      </svg>
+      
+      </span>
+
+      <!-- Dropdown list -->
+      <ul
+        class={`flex-col absolute bg-white p-4 rounded-lg mt-2 text-[20px] pr-30 font-medium gap-2 ${isDropdownVisible ? "block" : "hidden"}`}
+      >
+        <li
+          class="cursor-pointer flex items-center gap-2"
+          on:click={hideDropdown}
+        >
+          <input
+            id="email"
+            name="notification-method"
+            type="radio"
+            class="appearance-none h-7 w-7 border-2 bg-white rounded-full checked:bg-white checked:border-4 checked:border-indigo-600"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+            />
+          </svg>
+
+          <span>List View</span>
+        </li>
+        <li
+          class="cursor-pointer flex items-center gap-2"
+          on:click={hideDropdown}
+        >
+          <input
+            id="email"
+            name="notification-method"
+            type="radio"
+            class="appearance-none h-7 w-7 border-2 bg-white rounded-full checked:bg-white checked:border-4 checked:border-indigo-600"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+            />
+          </svg>
+
+          <span>Gallery View</span>
+        </li>
+        <li
+          class="cursor-pointer flex items-center gap-2"
+          on:click={hideDropdown}
+        >
+          <input
+            id="email"
+            name="notification-method"
+            type="radio"
+            class="appearance-none h-7 w-7 border-2 bg-white rounded-full checked:bg-white checked:border-4 checked:border-indigo-600"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+            />
+          </svg>
+
+          <span>Canlander View</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+
