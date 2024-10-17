c607a58f3efbc988dd32fc8867b294278d99cef7
Amjad-722 <aliamjad722ee@gmail.com>
2024-10-17 19:06:29 +0500

    updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,17 @@ module.exports = {
     "./src/routes/**/*.{html,js,svelte}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scaleAndMove: {
+          '0%, 100%': { transform: 'scale(0.75) translateX(0)' },
+          '80%': { transform: 'scale(1) translateX(1rem)' },
+        },
+      },
+      animation: {
+        'scale-move': 'scaleAndMove 2s ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
 };
